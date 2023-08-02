e7a97cbd080a098c7893605f8be323e1c9f1da5e
AKaya06 <ahmetkaya147@icloud.com>
2023-08-02 19:03:39 +0200

    Update Edit Password Generator.bat

--- Edit Password Generator.bat
+++ Edit Password Generator.bat
@@ -2,7 +2,6 @@
 REM This script requires the following environment variables to be set:
 REM 1. DEVENV_ROOT_2022
 REM 2. QTDIR_ROOT
-REM 3. LCR_ROOT
 
 IF "%DEVENV_ROOT_2022%" == "" (
 	ECHO Please specify the following environment variables: DEVENV_ROOT_2022
@@ -13,11 +12,6 @@ IF "%QTDIR_ROOT%" == "" (
 	ECHO Please specify the following environment variables: QTDIR_ROOT
 	goto PAUSE_END
 )
-
-REM IF "%LCR_ROOT%" == "" (
-REM	ECHO Please specify the following environment variables: LCR_ROOT
-REM	goto PAUSE_END
-REM )
 
 REM Setup eviroment
 SET QTDIR=%QTDIR_ROOT%
